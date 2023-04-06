f943516c44d00f18819f6e106f7e9262c7f4529a
H-EH7 <fly3767@gmail.com>
2023-04-07 00:05:18 +0900

    프로젝트 생성

---
+++ src/main/java/eh7/linkfolderapi/LinkFolderApiApplication.java
@@ -0,0 +1,13 @@
+package eh7.linkfolderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkFolderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkFolderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eh7/linkfolderapi/LinkFolderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eh7.linkfolderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkFolderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
